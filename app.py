5b0353c8f32c1af5b99dce864ea2510fd56f62f3
Rue <ruuue0000@gmail.com>
2025-05-11 00:32:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     with st.spinner("Detecting... Please wait"):
         img_array = np.array(image)
@@ -37,7 +37,7 @@ if uploaded_file:
 
     st.markdown("---")
     st.subheader("🧾 Detection Result")
-    st.image(result_img, caption="Processed Image", use_column_width=True)
+    st.image(result_img, caption="Processed Image", use_container_width=True)
     st.success("✅ Detection completed!")
 else:
     st.info("📤 Upload an image to get started.")
